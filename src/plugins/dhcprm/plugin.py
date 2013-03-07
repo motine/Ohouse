ec7e98368d23f8f56864d1b9af7663af8188021c
Tom Rothe <tom.rothe@eict.de>
2013-03-07 11:00:58 +0100

    fixed bootstrapping problem in dhcprm

--- src/plugins/dhcprm/plugin.py
+++ src/plugins/dhcprm/plugin.py
@@ -1,8 +1,5 @@
 import amsoil.core.pluginmanager as pm
 
-from dhcpresourcemanager import DHCPResourceManager
-import dhcpexceptions as exceptions_package
-
 def setup():
     # setup config keys
     config = pm.getService("config")
@@ -10,6 +7,8 @@ def setup():
     config.install("dhcprm.max_lease_duration", 24*60*60, "Maximum duration DHCP lease can be provisioned.")
     config.install("dhcprm.dbpath", "deploy/dhcp.db", "Path to the dhcp database (if relative, AMsoil's root will be assumed).")
     
+    from dhcpresourcemanager import DHCPResourceManager
+    import dhcpexceptions as exceptions_package
     rm = DHCPResourceManager()
     pm.registerService('dhcpresourcemanager', rm)
     pm.registerService('dhcpexceptions', exceptions_package)
