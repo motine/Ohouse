1fa9456c2c840ccf124f03f07291d21abe30a6d9
Tom Rothe <info@tomrothe.de>
2013-08-26 12:44:43 +0200

    auth fixes to ma

--- src/plugins/geni_trust/geniutil.py
+++ src/plugins/geni_trust/geniutil.py
@@ -13,6 +13,9 @@ import ext.sfa.trust.rights as sfa_rights
 import amsoil.core.log
 logger=amsoil.core.log.getLogger('geni_trust')
 
+import amsoil.core.pluginmanager as pm
+config = pm.getService('config')
+
 def decode_urn(urn):
     """Returns authority, type and name associated with the URN as string.
     example call:
