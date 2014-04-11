53f2fa4a786e450673ca64db3fe736afcf05d9cd
Adel Zaalouk <adel.zalok.89@gmail.com>
2014-04-11 11:40:11 +0200

    Removed Idea Files

--- src/plugins/ofed2/osavtwodelegate.py
+++ src/plugins/ofed2/osavtwodelegate.py
@@ -136,6 +136,7 @@ class OSAv2Delegate(GSAv2DelegateBase):
         membership for the given URN using the resource manager.
         """
         if (type_=='SLICE'):
+
             self._delegate_tools.member_check(['SLICE_MEMBER', 'SLICE_ROLE'],options)
             return self._slice_authority_resource_manager.modify_slice_membership(urn, certificate, credentials, options)
         elif (type_=='PROJECT'):
--- src/plugins/osliceauthorityrm/osliceauthorityresourcemanager.py
+++ src/plugins/osliceauthorityrm/osliceauthorityresourcemanager.py
@@ -127,6 +127,7 @@ class OSliceAuthorityResourceManager(object):
         Lookup a slice object.
         """
 
+        #self._delegate_tools.object_consistency_check('SLICE', match)
         return self._resource_manager_tools.object_lookup(self.AUTHORITY_NAME, 'slice', match, filter_)
 
     def create_sliver_info(self, client_cert, credentials, fields, options):
